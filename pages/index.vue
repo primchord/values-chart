b21c82ce8eb2628be7e0fb9f14863f750112dfa2
primchord <monica_no9@hotmail.co.jp>
2021-03-05 17:17:51 +0900

    add nuxt-hihgcharts

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="container">
     <div>
+      <highchart
+        :modules="['chart']"
+        :map="mapChart"
+        :options="chartOptions"
+        :update="watchers"
+      />
       <p>index</p>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      title: 'Highmaps (Africa) basic demo',
+      // mapChart: {
+      // It's important for mapName to match exactly
+      // chartOptions.chart.map below
+      // (since that's where the library renders to)
+      // mapName: 'africa',
+      // mapData: AfricaMapData,
+      // mapData: '/africa.geo.json' // Also works (if this is in static folder)
+      // },
+      watchers: ['options.title'],
+    }
+  },
+}
 </script>
 
 <style>
