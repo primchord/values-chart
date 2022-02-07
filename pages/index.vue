cd596f53d0be87c67f71aa866e27ede18f684149
primchord <monica_no9@hotmail.co.jp>
2022-02-07 15:50:29 +0900

    packedbubble setting

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="container">
-    <div>
-      <highchart
-        :modules="['chart']"
-        :map="mapChart"
-        :options="chartOptions"
-        :update="watchers"
-      />
-      <p>index</p>
-    </div>
+    <highchart
+      :options="chartOptions"
+      :update="['options.title', 'options.series']"
+      more
+    />
   </div>
 </template>
 
@@ -16,16 +12,91 @@
 export default {
   data() {
     return {
-      title: 'Highmaps (Africa) basic demo',
-      // mapChart: {
-      // It's important for mapName to match exactly
-      // chartOptions.chart.map below
-      // (since that's where the library renders to)
-      // mapName: 'africa',
-      // mapData: AfricaMapData,
-      // mapData: '/africa.geo.json' // Also works (if this is in static folder)
-      // },
-      watchers: ['options.title'],
+      chartOptions: {
+        tooltip: {
+          useHTML: true,
+          pointFormat: '<b>{point.name}:</b> {point.value}m CO<sub>2</sub>',
+        },
+        plotOptions: {
+          packedbubble: {
+            minSize: '30%',
+            maxSize: '120%',
+            zMin: 0,
+            zMax: 1000,
+            layoutAlgorithm: {
+              splitSeries: false,
+              gravitationalConstant: 0.02,
+            },
+            dataLabels: {
+              enabled: true,
+              format: '{point.name}',
+              filter: {
+                property: 'y',
+                operator: '>',
+                value: 250,
+              },
+              style: {
+                color: 'black',
+                textOutline: 'none',
+                fontWeight: 'normal',
+              },
+            },
+          },
+        },
+        chart: {
+          type: 'packedbubble',
+        },
+        title: {
+          text: 'Highmaps (Africa) basic demo',
+        },
+        series: [
+          {
+            name: 'Coffee', // Coffee series
+            data: [
+              {
+                // name property is used for the datalabel
+                // value property is used for the volume of the bubble
+                value: 12,
+                name: 'Bert',
+              },
+              {
+                value: 5,
+                name: 'John',
+              },
+              {
+                value: 10,
+                name: 'Sandra',
+              },
+              {
+                value: 7,
+                name: 'Cecile',
+              },
+            ],
+          },
+          {
+            name: 'Energy drinks', // Energy drinks series
+            data: [
+              {
+                value: 10,
+                name: 'Tristan',
+              },
+            ],
+          },
+          {
+            name: 'Tea', // Tea series
+            data: [
+              5,
+              6,
+              8,
+              {
+                value: 10,
+                name: 'Mustapha',
+                color: 'pink',
+              },
+            ],
+          },
+        ],
+      },
     }
   },
 }
@@ -39,5 +110,46 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+}
+.highcharts-data-table table {
+  min-width: 320px;
+  max-width: 800px;
+  margin: 1em auto;
+}
+
+.highcharts-data-table table {
+  font-family: Verdana, sans-serif;
+  border-collapse: collapse;
+  border: 1px solid #ebebeb;
+  margin: 10px auto;
+  text-align: center;
+  width: 100%;
+  max-width: 500px;
+}
+
+.highcharts-data-table caption {
+  padding: 1em 0;
+  font-size: 1.2em;
+  color: #555;
+}
+
+.highcharts-data-table th {
+  font-weight: 600;
+  padding: 0.5em;
+}
+
+.highcharts-data-table td,
+.highcharts-data-table th,
+.highcharts-data-table caption {
+  padding: 0.5em;
+}
+
+.highcharts-data-table thead tr,
+.highcharts-data-table tr:nth-child(even) {
+  background: #f8f8f8;
+}
+
+.highcharts-data-table tr:hover {
+  background: #f1f7ff;
 }
 </style>
